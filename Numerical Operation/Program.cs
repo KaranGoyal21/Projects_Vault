d75c74ddd33c78c0d085b634fff7347637b6a501
Karan Goyal <karangoyalsit@gmail.com>
2021-05-25 14:44:48 +0530

    reformatting

--- Numerical Operation/Program.cs
+++ Numerical Operation/Program.cs
@@ -6,8 +6,9 @@ namespace Numerical_Operation
     {
         static void Main(string[] args)
         {
-            GetEvenOdd();
-            /*
+             /*
+             GetEvenOdd();
+
              AccountLogin();
 
              GetGrade();
@@ -36,10 +37,11 @@ namespace Numerical_Operation
              */
         }
 
+        /// <summary>
+        /// To check if the user given input integer value is Even or Odd in nature.
+        /// </summary>
         static void GetEvenOdd()
         {
-            // to check if the user given input integer value is Even or Odd in nature. 
-
             int num = int.Parse(Console.ReadLine());
 
             if (num % 2 == 0)
@@ -52,9 +54,11 @@ namespace Numerical_Operation
             }
         }
 
+        /// <summary>
+        /// To login the user with specific username & password only, any wrong value should be displayed with invalid inputs.
+        /// </summary>
         static void AccountLogin()
         {
-            // to login the user with specific username & password only, any wrong value should be displayed with invalid inputs.
 
             string username = Console.ReadLine();
             var password = Console.ReadLine();
@@ -69,10 +73,11 @@ namespace Numerical_Operation
             }
         }
 
+        /// <summary>
+        /// To get inputs from user and convert it into GRADE A,B,C,D format with percentage range of 100-80,80-60,60-40,40-0 respectively.
+        /// </summary>
         static void GetGrade()
         {
-            // to get inputs from user and convert it into GRADE A,B,C,D format with percentage range of 100-80,80-60,60-40,40-0 respectively.
-
             int num = int.Parse(Console.ReadLine());
 
             if (num >= 0 && num <= 100)
@@ -100,10 +105,11 @@ namespace Numerical_Operation
             }
         }
 
+        /// <summary>
+        /// get character input form user and state whether it is an vowel or not
+        /// </summary>
         static void CheckVowel()
         {
-            // get character input form user and state whether it is an vowel or not
-
             char ch = char.Parse(Console.ReadLine());
             string s = ch.ToString().ToLower();
 
@@ -126,12 +132,11 @@ namespace Numerical_Operation
             }
         }
 
+        /// <summary>
+        /// To increment the counter from zero to 10. It should also display the even odd number along with the increment value.
+        /// </summary>
         static void GetLoopEvenOddValues()
         {
-            // to increment the counter from zero to 10. It should also display the even odd number along with the increment value.
-
-
-
             for (int i = 1; i <= 10; i++)
             {
 
@@ -146,10 +151,11 @@ namespace Numerical_Operation
             }
         }
 
+        /// <summary>
+        /// get inputs form user and showcase whether it is a prime number or not using square root
+        /// </summary>
         static void CheckprimeValueType1()
         {
-            // get inputs form user and showcase whether it is a prime number or not using square root
-
             int num = int.Parse(Console.ReadLine());
             bool isprime = true;
 
@@ -175,10 +181,11 @@ namespace Numerical_Operation
 
         }
 
+        /// <summary>
+        /// get inputs form user and showcase whether it is a prime number or not using for condition
+        /// </summary>
         static void CheckprimeValueType2()
         {
-            // get inputs form user and showcase whether it is a prime number or not
-
             int num = int.Parse(Console.ReadLine());
             bool isprime = true;
             for (int i = 2; i < num; i++)
@@ -201,6 +208,9 @@ namespace Numerical_Operation
             }
         }
 
+        /// <summary>
+        /// get inputs form user and showcase whether it is a prime number or not using while condition
+        /// </summary>
         static void CheckprimeValueType3()
         {
             bool isprime = true;
@@ -228,10 +238,11 @@ namespace Numerical_Operation
             }
         }
 
+        /// <summary>
+        /// showcase first 100 prime numbers
+        /// </summary>
         static void First100PrimeNumbers()
         {
-            // showcase 100 prime numbers
-
             for (int num = 1; num <= 100; num++)
             {
                 bool isprime = true;
@@ -250,11 +261,11 @@ namespace Numerical_Operation
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// get input number from user and showing table of that number till 10 values
+        /// </summary>
         static void DisplayTable()
         {
-            // get input number from user and showing table of that number till 10 values
-
-
             int num = int.Parse(Console.ReadLine());
             for (int i = 1; i <= 10; i++)
             {
@@ -263,10 +274,11 @@ namespace Numerical_Operation
             }
         }
 
+        /// <summary>
+        /// get input number from user and showing table of number till you reach the input number table
+        /// </summary>
         static void DisplayTableTillGivenValue()
         {
-            // get input number from user and showing table of number till you reach the input number table
-
             int num = int.Parse(Console.ReadLine());
             for (int a = 1; a <= num; a++)
             {
@@ -280,10 +292,11 @@ namespace Numerical_Operation
             }
         }
 
+        /// <summary>
+        /// calculating number of digits from given number from user
+        /// </summary>
         static void CalculateDigits()
         {
-            // calculating number of digits from given number from user
-
             int a = int.Parse(Console.ReadLine());
             int temp = a;
             int digit = 0;
@@ -296,10 +309,11 @@ namespace Numerical_Operation
             Console.WriteLine("{0} is {1} digit number", a, digit);
         }
 
+        /// <summary>
+        /// accept only three attempts of input pin from user or else block the account
+        /// </summary>
         static void LimitPinAttempts()
         {
-            // accept only three attempts of input pin from user or else block the account
-
             string actualpin = "1234";
             string pinbyuser;
             int count = 0;
@@ -321,10 +335,11 @@ namespace Numerical_Operation
             }
         }
 
+        /// <summary>
+        /// accept only three attempts of input pin from user, show the number of failed attempts and after 3 attempts block the account
+        /// </summary>
         static void RetryPinAttempts()
         {
-            // accept only three attempts of input pin from user, show the number of failed attempts and after 3 attempts block the account
-
             string actualpin = "9876";
             string pinbyuser;
             int count = 0;
@@ -350,11 +365,11 @@ namespace Numerical_Operation
             }
         }
 
+        /// <summary>
+        /// fizz buzz program in which multiple of 3,5 will be displayed fizz and buzz respectively and number which is multiple of both will be displayed as fizzbuzz
+        /// </summary>
         static void FizzBuzzProgramType1()
         {
-
-            // fizz buzz program in which multiple of 3,5 will be displayed fizz and buzz respectively and number which is multiple of both will be displayed as fizzbuzz
-
             for (int i = 1; i <= 100; i++)
             {
                 if (i % 15 == 0)
@@ -379,11 +394,11 @@ namespace Numerical_Operation
             }
         }
 
+        /// <summary>
+        /// fizz buzz program in which multiple of 3,5 will be displayed fizz and buzz respectively and number which is multiple of both will be displayed as fizzbuzz
+        /// </summary>
         static void FizzBuzzProgramType2()
         {
-
-            // fizz buzz program in which multiple of 3,5 will be displayed fizz and buzz respectively and number which is multiple of both will be displayed as fizzbuzz
-
             string output = null;
             int counter = 1;
             while (counter <= 100)
