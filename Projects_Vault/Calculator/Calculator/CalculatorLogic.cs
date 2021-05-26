b851ad1fba584000e7706dc5c45e6dfc4f2e9b37
Karan Goyal <karangoyalsit@gmail.com>
2021-05-26 10:47:49 +0530

    Adding calculator project

---
+++ Projects_Vault/Calculator/Calculator/CalculatorLogic.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class CalculatorLogic
+    {
+
+        double value1;
+        double value2;
+
+        public void AcceptValues()
+        {
+            //value1 = 5;
+            //value2 = 10;
+            Console.Write("Value1 Input: ");
+            value1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Value2 Input: ");
+            value2 = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public double GetAddition()
+        {
+            return value1 + value2;
+        }
+
+        public double GetAddition(double val1, double val2)
+        {
+            return val1 + val2;
+        }
+
+        public double GetSubtraction()
+        {
+            return value1 - value2;
+        }
+
+        public double GetMultiplication()
+        {
+            return value1 * value2;
+        }
+
+        public double GetDivision()
+        {
+            return value1 / value2;
+        }
+
+        
+
+
+
+    }
+
+
+}
---
+++ Projects_Vault/Calculator/Calculator/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Calculator
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string continueCalculator = "";
+            CalculatorLogic c = new CalculatorLogic();
+
+            do
+            {
+                Console.Clear();
+                DisplayOperations();
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                c.AcceptValues();
+
+                if (x == 1)
+                {
+                    double y = c.GetAddition();
+                    //double y = c.GetAddition(5, 6);
+                    Console.WriteLine("Addition of values are: {0}", y);
+                }
+                else if (x == 2)
+                {
+                    double y = c.GetSubtraction();
+                    Console.WriteLine("Subtraction of values are: {0} ", y);
+                }
+                else if (x == 3)
+                {
+                    double y = c.GetMultiplication();
+                    Console.WriteLine("Multiplication of values are: {0} ", y);
+                }
+                else if (x == 4)
+                {
+                    double y = c.GetDivision();
+                    Console.WriteLine("Division of values are: {0} ", y);
+                }
+
+                ContinueOperation();
+                continueCalculator = Console.ReadLine().ToLower();
+                
+
+            } while (continueCalculator == "y");
+
+        }
+
+        static void DisplayOperations()
+        {
+            Console.WriteLine("Select desired operation");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.Write("Operation Input: ");
+        }
+
+        static void ContinueOperation()
+        {
+            Console.WriteLine("\n\nDo you want to continue");
+            Console.WriteLine("\nPress Y for yes :");
+            Console.WriteLine("Press N for no :");
+        }
+    }
+}
