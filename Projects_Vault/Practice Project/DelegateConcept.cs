cd3169ff1531bf739f96974337cfb603471a9213
Karan Goyal <karangoyalsit@gmail.com>
2021-06-07 12:24:53 +0530

    new codes

---
+++ Projects_Vault/Practice Project/DelegateConcept.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Project
+{
+    /// <summary>
+    /// Delegate concept
+    /// It adds reference of method in a delegate for further sequential task.
+    /// Multiple methods in single delegate is known as Multiple Cast Delegate.
+    /// </summary>
+    /*
+    delegate int Mydel(int x, int y); // For single reference delegate
+    */
+    delegate void Mydel(int x, int y); // For MultiCast Delegate
+    class Program
+    {
+        // For single reference delegate
+        /*
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        */
+
+        // For MultiCast Delegate
+        public static void Add(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public static void Multiply(int x, int y)
+        {
+            Console.WriteLine(x * y);
+        }
+
+        static void Main(string[] args)
+        {
+            // For single reference delegate
+            /*
+            Mydel del = new Mydel(Add);
+            int result = del(10, 30);
+            Console.WriteLine(result);
+            del = Multiply;
+            result = del(10, 30);
+            Console.WriteLine(result);
+            */
+
+            // For MultiCast Delegate
+            Mydel del = Add;
+            del += Multiply;
+            del(10, 30);
+
+        }
+    }
+}
---
+++ Projects_Vault/Practice Project/GenericDelegates.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Project
+{
+    /// <summary>
+    /// Generic Delegate concept
+    /// Defining data type at runtime.
+    /// Using some pre defined generic delegate i.e func,action,predicate
+    /// </summary>
+
+    class Program
+    {
+        public static void SampleMethod(string name)
+        {
+            Console.WriteLine("Welcome " + name);
+        }
+        public static int Add(int x,int y)
+        {
+            return x + y;
+        }
+        public static bool Login(string uid)
+        {
+            if(uid == "karan")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Action<string> action1 = SampleMethod;
+            Func<int, int, int> func1 = Add;
+            Predicate<string> pred1 = Login;
+
+            action1("Karan");
+            int result = func1(10, 20);
+            bool result2 = pred1("karan");
+
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+        }
+    }
+}
