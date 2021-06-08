320cc0e5d662a2722625854b07c097cca899aeb2
Karan Goyal <karangoyalsit@gmail.com>
2021-06-08 19:11:04 +0530

    Student management system

---
+++ Projects_Vault/Student_Management_System/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Student_Management_System
+{
+    class Student
+    {
+        public int Standard { get; set; }
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Height { get; set; }
+        public string Address { get; set; }
+
+        public Student()
+        { }
+
+        public Student(int a, int b, string c, int d, double e, string f)
+        {
+            this.Standard = a;
+            this.RollNo = b;
+            this.Name = c;
+            this.Age = d;
+            this.Height = e;
+            this.Address = f;
+        }
+
+        public Student(string a, int b, int c)
+        {
+            this.Name = a;
+            this.Age = b;
+            this.Standard = c;
+        }
+
+        public Student(string a, int b)
+        {
+            this.Name = a;
+            this.Age = b;
+        }
+    }
+}
