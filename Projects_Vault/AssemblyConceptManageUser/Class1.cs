dce823f079703e558d33aa66ed27e80aab6975e9
karangoyalsit@gmail.com <karangoyalsit@gmail.com>
2021-06-24 18:13:46 +0530

    private assembly

---
+++ Projects_Vault/AssemblyConceptManageUser/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AssemblyConceptManageUser
+{
+    /// <summary>
+    /// Assembly Concept.
+    /// It is basic structure to create or modify application developed in c#.
+    /// It is also used for deployment and configurational things.
+    /// .EXE/.DLL are known as assembly.
+    /// Two types of assembly i.e private & shared.
+    /// Private assembly referencing creates the copy of dll/exe assembly in your project.
+    /// Shared assembly is created by installing assembly in GAC(global assembly cache) through which multiple referncing can be done.
+    /// Copy of assembly is not created in shared assembly.
+    /// </summary>
+
+    public class User
+    {
+        public string HelloUser(string user)
+        {
+            return "Hello" + user;
+        }
+
+        public bool AuthenticateUser(string user, string password)
+        {
+            if (user == "karan" && password == "12345")
+                return true;
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+        }
+
+    }
