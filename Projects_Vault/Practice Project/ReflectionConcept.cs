ac91741752920897dc084edaa9a696dbd0968d2c
karangoyalsit@gmail.com <karangoyalsit@gmail.com>
2021-06-24 20:45:54 +0530

    Reflection concept

--- Projects_Vault/Practice Project/ReflectionConcept.cs
+++ Projects_Vault/Practice Project/ReflectionConcept.cs
@@ -4,8 +4,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Practice_Project
 {
+    /// <summary>
+    /// Reflection Concept      (ildasm (dll file name) in cmd prmt)
+    /// If we want a structure or any detailed information about a particular assembly or a type.
+    /// It is used to obtain type information at runtime
+    /// </summary>
+    class program
+    {
+        static void Main(string[] args)
+        {
+            /*    If dll is available
+            
+            string path = @"E:\study\study\projects\Projects_Vault\Projects_Vault\AssemblyConceptManageUser\bin\Debug\net5.0\AssemblyConceptManageUser.dll";
 
+            Assembly assembly = Assembly.LoadFile(path);
+
+            Type[] types = assembly.GetTypes();
+            foreach (var type in types)
+            {
+                Console.WriteLine("Class : " + type.Name);
+
+                MethodInfo[] methods = type.GetMethods();
+                foreach (var method in methods)
+                {
+                    Console.WriteLine("-- Method:" + method.Name);
+
+                    ParameterInfo[] parameters = method.GetParameters();
+                    foreach (var parameter in parameters)
+                    {
+                        Console.WriteLine("---- Parameter: " + parameter.Name + " : " + parameter.ParameterType);
+                    }
+                }
+            }*/
+
+            // if we want to pass particular type name
+
+            Type type = typeof(string);
+
+            Console.WriteLine("Class : " + type.Name);
+
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (var prop in properties)
+            {
+                Console.WriteLine("-- Property: " + prop);
+            }
+
+            //MethodInfo[] methods = type.GetMethods();
+            //foreach (var method in methods)
+            //{
+            //    Console.WriteLine("-- Method:" + method.Name);
+
+            //    ParameterInfo[] parameters = method.GetParameters();
+            //    foreach (var parameter in parameters)
+            //    {
+            //        Console.WriteLine("---- Parameter: " + parameter.Name + " : " + parameter.ParameterType);
+            //    }
+            //}
+
+
+        }
+    }
 }
